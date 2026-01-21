a5c65158aa642493fb5c49cbe4d2e3717b942c4a
Faviun <lightbringerlal@gmail.com>
2026-01-21 14:46:25 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Auth() {
+  return (
+    <>
+      <Head>
+        <title>Auth Page</title>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="description" content="This is the authentication page" />
+        <link rel="icon" type="image/svg" sizes="32x32" href="/img/logo.svg" />
+      </Head>
+      <h1>Authentication Page</h1>
+    </>
+  )
+}
